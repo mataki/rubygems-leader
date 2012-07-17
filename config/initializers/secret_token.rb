fc3cf7c9e09cd1eb25378a16eb41b59a5f4edb28
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-17 19:59:46 +0900

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubygemsLeader::Application.config.secret_token = '06d0ab6bb5288c122b9452a5858651247a701f0ac22db7a1711c642a79f1a5e91c86edeaa4c3bc1da8036361583b78b409ff6783c9f0e997d4f23848547bb81e'
