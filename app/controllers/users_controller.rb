d696a959c620457d9ea04caf0b9560d011ad2da5
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-18 03:24:46 +0900

    Fix a bit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.order("rank ASC").page(params[:page])
+    @users = User.order("rank ASC, total_downloads DESC").page(params[:page])
 
     respond_to do |format|
       format.html # index.html.erb
