0614c2d8d947ce4836c4fae0cfd4b4d3922138c7
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-17 21:33:55 +0900

    Add analytics tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,24 @@ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
 EOF
     code.html_safe
   end
+
+  def analytics
+    return unless Rails.env.production?
+    code = <<-EOF
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-33450752-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+EOF
+    code.html_safe
+  end
 end
