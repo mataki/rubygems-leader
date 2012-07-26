9ee3a5d1f16a8baf94893601403dba4fb3623793
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-27 03:32:50 +0900

    Fix link url at fork me on github

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,7 +55,7 @@ EOF
 
   def forkmeongithub
     code = <<-EOF
-<a href="https://github.com/you"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 1050;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
+<a href="https://github.com/mataki/rubygems-leader" target="_blank"><img style="position: absolute; top: 0; right: 0; border: 0; z-index: 1050;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_green_007200.png" alt="Fork me on GitHub"></a>
 EOF
     code.html_safe
   end
