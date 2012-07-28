7c4f239f3a570ed94cb928a44dc7616738cf7d66
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-29 08:30:58 +0900

    Use letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ RubygemsLeader::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :letter_opener
 end
