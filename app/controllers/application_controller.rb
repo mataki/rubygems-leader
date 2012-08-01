f7d9ba768f6b668782d616026a516799986603df
Randy Morgan <digital.ipseity@gmail.com>
2012-08-02 00:17:58 +0900

    one more time if you please

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_filter :miniprofiler
 
+  def auth_failure
+    redirect_to root_path, alert: params[:message]
+  end
+
   private
 
   def miniprofiler
     Rack::MiniProfiler.authorize_request if params[:miniprofiler].present?
   end
 
-  def auth_failure
-    redirect_to root_path, alert: params[:message]
-  end
-
-  def current_user
+ def current_user
     return unless session[:user_id]
     @current_user ||= User.find(session[:user_id])
   end
