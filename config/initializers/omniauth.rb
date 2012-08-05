3b45e30b102d581818c140f2d3ff5bc26453fdad
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-08-05 12:13:18 +0900

    set ENV setting on .powenv

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,12 +4,11 @@ unless Rails.env.production?
   omniauth_yaml = File.expand_path('../../oauth_github.yml', __FILE__)
   if File.exists?(omniauth_yaml)
     omni_data = HashWithIndifferentAccess.new(YAML.load(File.read(omniauth_yaml)))
-    omni_data.each do |key, value| 
+    omni_data.each do |key, value|
       ENV[key.to_s.upcase] = value
     end
   end
 end
-
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], skip_info: true
@@ -17,4 +16,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     OmniAuth::FailureEndpoint.new(env).redirect_to_failure
   }
 end
-
